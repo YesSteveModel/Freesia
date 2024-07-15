af0fe75388ba73d914e0dac8375738a72669bdf3
MrHua269 <wangxyper@163.com>
2024-07-15 18:01:24 +0800

    Do not save chunks because we don't need it

--- Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/MinecraftServerMixin.java
+++ Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/MinecraftServerMixin.java
@@ -2,9 +2,12 @@ package gg.earthme.cyanidin.cyanidinworker.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.BooleanSupplier;
 
@@ -15,7 +18,12 @@ public class MinecraftServerMixin {
      * @reason Kill the ticking
      */
     @Redirect(method = "tickChildren", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;tick(Ljava/util/function/BooleanSupplier;)V"))
-    public void tickLevel(ServerLevel instance, BooleanSupplier booleanSupplier) {
+    public void tickLevelHook(ServerLevel instance, BooleanSupplier booleanSupplier) {
 
+    }
+
+    @Inject(method = "saveEverything", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;saveAllChunks(ZZZ)Z"))
+    public void saveAllChunksHook(boolean bl, boolean bl2, boolean bl3, @NotNull CallbackInfoReturnable<Boolean> cir){
+        cir.cancel();
     }
 }
