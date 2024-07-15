fd1d1c1b37c24f1636b5465c95fa6ee74732ff52
MrHua269 <wangxyper@163.com>
2024-07-15 17:24:16 +0800

    Adjusted packet filters

--- Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/ConnectionMixin.java
+++ Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/ConnectionMixin.java
@@ -4,11 +4,12 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.*;
-import net.minecraft.network.protocol.configuration.ClientboundFinishConfigurationPacket;
-import net.minecraft.network.protocol.configuration.ClientboundRegistryDataPacket;
-import net.minecraft.network.protocol.configuration.ClientboundSelectKnownPacks;
+import net.minecraft.network.protocol.configuration.*;
 import net.minecraft.network.protocol.game.*;
+import net.minecraft.network.protocol.login.ClientboundGameProfilePacket;
 import net.minecraft.network.protocol.login.ClientboundHelloPacket;
+import net.minecraft.network.protocol.login.ClientboundLoginCompressionPacket;
+import net.minecraft.network.protocol.login.ClientboundLoginDisconnectPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,14 +20,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ConnectionMixin {
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At(value = "HEAD"), cancellable = true)
     public void sendPacketPInject(Packet<?> packet, CallbackInfo ci){
-        if (this.checkPacket(packet)){
+        if (!this.checkPacket(packet)){
             ci.cancel();
         }
     }
 
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;)V", at = @At(value = "HEAD"), cancellable = true)
     public void sendPacketPCInject(Packet<?> packet, PacketSendListener packetSendListener, CallbackInfo ci){
-        if (this.checkPacket(packet)){
+        if (!this.checkPacket(packet)){
             if (packetSendListener != null){
                 packetSendListener.onSuccess();
             }
@@ -36,7 +37,7 @@ public class ConnectionMixin {
 
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;Z)V", at = @At(value = "HEAD"), cancellable = true)
     public void sendPacketPCBInject(Packet<?> packet, PacketSendListener packetSendListener, boolean bl, CallbackInfo ci){
-        if (this.checkPacket(packet)){
+        if (!this.checkPacket(packet)){
             if (packetSendListener != null){
                 packetSendListener.onSuccess();
             }
@@ -46,22 +47,20 @@ public class ConnectionMixin {
 
     @Unique
     public boolean checkPacket(Packet<?> pkt) {
-        return pkt instanceof ClientboundSetTimePacket
-                || pkt instanceof ClientboundAddEntityPacket
-                || pkt instanceof ClientboundEntityEventPacket
-                || pkt instanceof ClientboundSetEntityMotionPacket
-                || pkt instanceof ClientboundMoveEntityPacket
-                || pkt instanceof ClientboundSoundEntityPacket
+        return pkt instanceof ClientboundLoginCompressionPacket
+                || pkt instanceof ClientboundHelloPacket
+                || pkt instanceof ClientboundGameProfilePacket
+                || pkt instanceof ClientboundCustomPayloadPacket
+                || pkt instanceof ClientboundPingPacket
+                || pkt instanceof ClientboundFinishConfigurationPacket
+                || pkt instanceof ClientboundLoginPacket
+                || pkt instanceof ClientboundLoginDisconnectPacket
+                || pkt instanceof ClientboundStartConfigurationPacket
                 || pkt instanceof ClientboundRegistryDataPacket
-                || pkt instanceof ClientboundChangeDifficultyPacket
-                || pkt instanceof ClientboundPlayerAbilitiesPacket
+                || pkt instanceof ClientboundUpdateEnabledFeaturesPacket
+                || pkt instanceof ClientboundSelectKnownPacks
                 || pkt instanceof ClientboundUpdateTagsPacket
-                || pkt instanceof ClientboundUpdateRecipesPacket
-                || pkt instanceof ClientboundSetHealthPacket
-                || pkt instanceof ClientboundLevelChunkWithLightPacket
-                || pkt instanceof ClientboundSetExperiencePacket
-                || pkt instanceof ClientboundChunkBatchStartPacket
-                || pkt instanceof ClientboundSetChunkCacheCenterPacket
-                || pkt instanceof ClientboundContainerSetDataPacket;
+                || pkt instanceof ClientboundResetChatPacket
+                || pkt instanceof ClientboundKeepAlivePacket;
     }
 }
