8f344813917efa3ae8412eb381035f035af9b390
MrHua269 <wangxyper@163.com>
2024-08-05 19:08:23 +0800

    Upgraded to ysm 2.2.2

--- Cyanidin-Velocity/src/main/java/gg/earthme/cyanidin/cyanidin/network/ysm/YsmMapperPayloadManager.java
+++ Cyanidin-Velocity/src/main/java/gg/earthme/cyanidin/cyanidin/network/ysm/YsmMapperPayloadManager.java
@@ -22,8 +22,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class YsmMapperPayloadManager {
-    public static final Key YSM_CHANNEL_KEY_ADVENTURE = Key.key("yes_steve_model:1_2_1");
-    public static final MinecraftChannelIdentifier YSM_CHANNEL_KEY_VELOCITY = MinecraftChannelIdentifier.create("yes_steve_model", "1_2_1");
+    public static final Key YSM_CHANNEL_KEY_ADVENTURE = Key.key("yes_steve_model:2_2_2");
+    public static final MinecraftChannelIdentifier YSM_CHANNEL_KEY_VELOCITY = MinecraftChannelIdentifier.create("yes_steve_model", "2_2_2");
 
     private final Map<Player, TcpClientSession> player2Mappers = new ConcurrentHashMap<>();
     private final Map<Player, MapperSessionProcessor> mapperSessions = new ConcurrentHashMap<>();
