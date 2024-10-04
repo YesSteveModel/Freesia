0e5d06a1d9910f598ebd7dcc017f5581365d2a27
MrHua269 <wangxyper@163.com>
2024-10-04 09:19:27 +0800

    Force disable packet compressors

---
+++ Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/impl/FakeCompressionEncoder.java
@@ -0,0 +1,19 @@
+package gg.earthme.cyanidin.cyanidinworker.impl;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import net.minecraft.network.VarInt;
+
+public class FakeCompressionEncoder extends MessageToByteEncoder<ByteBuf> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
+        int i = msg.readableBytes();
+        if (i > 8388608) {
+            throw new IllegalArgumentException("Packet too big (is " + i + ", should be less than 8388608)");
+        } else {
+            VarInt.write(out, 0);
+            out.writeBytes(msg);
+        }
+    }
+}
--- Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/ConnectionMixin.java
+++ Cyanidin-Worker/src/main/java/gg/earthme/cyanidin/cyanidinworker/mixin/ConnectionMixin.java
@@ -1,5 +1,9 @@
 package gg.earthme.cyanidin.cyanidinworker.mixin;
 
+import gg.earthme.cyanidin.cyanidinworker.impl.FakeCompressionEncoder;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import net.minecraft.network.CompressionDecoder;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
@@ -11,6 +15,8 @@ import net.minecraft.network.protocol.login.ClientboundHelloPacket;
 import net.minecraft.network.protocol.login.ClientboundLoginCompressionPacket;
 import net.minecraft.network.protocol.login.ClientboundLoginDisconnectPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,6 +24,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Connection.class)
 public class ConnectionMixin {
+
+    @Shadow private Channel channel;
+
+    /**
+     * @author MrHua269
+     * @reason Force disable packet compressors
+     */
+    @Overwrite
+    public void setupCompression(int i, boolean bl) {
+        if (i >= 0) {
+            ChannelHandler var4 = this.channel.pipeline().get("decompress");
+            if (var4 instanceof CompressionDecoder compressionDecoder) {
+                compressionDecoder.setThreshold(i, bl);
+            } else {
+                this.channel.pipeline().addAfter("splitter", "decompress", new CompressionDecoder(i, bl));
+            }
+
+            var4 = this.channel.pipeline().get("compress");
+
+            if (!(var4 instanceof FakeCompressionEncoder)) {
+                this.channel.pipeline().addAfter("prepender", "compress", new FakeCompressionEncoder());
+            }
+        } else {
+            if (this.channel.pipeline().get("decompress") instanceof CompressionDecoder) {
+                this.channel.pipeline().remove("decompress");
+            }
+
+            if (this.channel.pipeline().get("compress") instanceof FakeCompressionEncoder) {
+                this.channel.pipeline().remove("compress");
+            }
+        }
+
+    }
+
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At(value = "HEAD"), cancellable = true)
     public void sendPacketPInject(Packet<?> packet, CallbackInfo ci){
         if (!this.checkPacket(packet)){
