1a38482ac8bd95d6a8e0869ca8e12f36145413c0
MrHua269 <mrhua269@gmail.com>
2025-03-03 11:36:53 +0800

    Removed useless connection creation callback queue

--- Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/network/ysm/YsmMapperPayloadManager.java
+++ Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/network/ysm/YsmMapperPayloadManager.java
@@ -25,10 +25,8 @@ import java.net.InetSocketAddress;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class YsmMapperPayloadManager {
@@ -43,8 +41,6 @@ public class YsmMapperPayloadManager {
     // Player to worker mappers connections
     private final Map<Player, TcpClientSession> player2Mappers = new ConcurrentHashMap<>();
     private final Map<Player, MapperSessionProcessor> mapperSessions = new ConcurrentHashMap<>();
-    // Creation callbacks
-    private final Map<Player, Queue<Consumer<MapperSessionProcessor>>> mapperCreateCallbacks = new ConcurrentHashMap<>();
 
     // Backend connect infos
     private final ReadWriteLock backendIpsAccessLock = new ReentrantReadWriteLock(false);
@@ -247,10 +243,6 @@ public class YsmMapperPayloadManager {
         return this.player2Mappers.containsKey(player);
     }
 
-    public void onPlayerConnected(Player player) {
-        this.mapperCreateCallbacks.putIfAbsent(player, new ConcurrentLinkedQueue<>());
-    }
-
     public void firstCreateMapper(Player player) {
         this.createMapperSession(player, Objects.requireNonNull(this.selectLessPlayer()));
     }
@@ -263,18 +255,6 @@ public class YsmMapperPayloadManager {
         this.ysmInstalledPlayers.remove(player);
 
         final MapperSessionProcessor mapperSession = this.mapperSessions.remove(player);
-        final Queue<Consumer<MapperSessionProcessor>> removedQueue = this.mapperCreateCallbacks.remove(player);
-
-        if (removedQueue != null) {
-            Consumer<MapperSessionProcessor> unprocessed;
-            while ((unprocessed = removedQueue.poll()) != null) {
-                try {
-                    unprocessed.accept(mapperSession);
-                } catch (Exception e) {
-                    Freesia.LOGGER.error("Failed to retire connect callback!", e);
-                }
-            }
-        }
 
         if (mapperSession != null) {
             mapperSession.setKickMasterWhenDisconnect(false); //Player already offline, so we don't disconnect again
@@ -290,20 +270,6 @@ public class YsmMapperPayloadManager {
             mapperSession.getBindPlayer().disconnect(Freesia.languageManager.i18n("cyanidin.backend.disconnected", List.of("reason"), List.of(reason)));
         this.player2Mappers.remove(mapperSession.getBindPlayer());
         this.mapperSessions.remove(mapperSession.getBindPlayer());
-
-        final Queue<Consumer<MapperSessionProcessor>> removedQueue = this.mapperCreateCallbacks.get(mapperSession.getBindPlayer());
-
-        //Finalize the callbacks
-        if (removedQueue != null) {
-            Consumer<MapperSessionProcessor> unprocessed;
-            while ((unprocessed = removedQueue.poll()) != null) {
-                try {
-                    unprocessed.accept(mapperSession);
-                } catch (Exception e) {
-                    Freesia.LOGGER.error("Failed to retire connect callback!", e);
-                }
-            }
-        }
     }
 
     public void onPluginMessageIn(@NotNull Player player, @NotNull MinecraftChannelIdentifier channel, byte[] packetData) {
@@ -349,17 +315,6 @@ public class YsmMapperPayloadManager {
 
     public void onProxyLoggedin(Player player, MapperSessionProcessor packetProcessor, TcpClientSession session){
         this.player2Mappers.put(player, session);
-        //Finish the callbacks
-        Freesia.PROXY_SERVER.getScheduler().buildTask(Freesia.INSTANCE, () -> {
-            Consumer<MapperSessionProcessor> callback;
-            while ((callback = this.mapperCreateCallbacks.get(player).poll()) != null) {
-                try {
-                    callback.accept(packetProcessor);
-                } catch (Exception e) {
-                    Freesia.LOGGER.info("Error occurs while processing connect callbacks!", e);
-                }
-            }
-        }).schedule();
     }
 
     public void onVirtualPlayerTrackerUpdate(UUID owner, Player watcher) {
@@ -378,16 +333,11 @@ public class YsmMapperPayloadManager {
     public void onRealPlayerTrackerUpdate(Player owner, Player watcher) {
         final MapperSessionProcessor mapperSession = this.mapperSessions.get(owner);
 
+        // The mapper was created earlier than the player's connection turned in-game state
+        // so as the result, we could simply pass it down directly
         if (mapperSession == null) {
-            //Commit to callback if the mapper session of the player not finished connecting currently
-            this.mapperCreateCallbacks.computeIfAbsent(owner, player -> new ConcurrentLinkedQueue<>()).offer((mapper) -> {
-                if (mapper == null) {
-                    return;
-                }
-
-                mapper.getPacketProxy().sendEntityStateTo(watcher);
-            });
-            return;
+            // Should not be happened
+            throw new IllegalStateException("???");
         }
 
         if (this.isPlayerInstalledYsm(watcher)) {
