85cdfa1cd3732cd3155037686bcb8d74607b8f96
MrHua269 <mrhua269@gmail.com>
2025-03-03 12:42:53 +0800

    Removed useless casts

--- Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/Freesia.java
+++ Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/Freesia.java
@@ -157,7 +157,7 @@ public class Freesia implements PacketListener {
     public void onPacketSend(@NotNull PacketSendEvent event) {
         if (event.getPacketType() == PacketType.Play.Server.JOIN_GAME) {
             final WrapperPlayServerJoinGame playerSpawnPacket = new WrapperPlayServerJoinGame(event);
-            final Player target = (Player) event.getPlayer();
+            final Player target = event.getPlayer();
 
             logger.info("Entity id update for player {} to {}", target.getUsername(), playerSpawnPacket.getEntityId());
             mapperManager.updateRealPlayerEntityId(target, playerSpawnPacket.getEntityId());
