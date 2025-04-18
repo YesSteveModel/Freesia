919172c352db70889c362920cb19541f47ffc596
MrHua269 <mrhua269@gmail.com>
2025-04-18 19:36:11 +0800

    Fully rewrote packet proxy, cleaned codes and reuse BuildConstants

--- Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/network/ysm/VirtualYsmPacketProxyImpl.java
+++ Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/network/ysm/VirtualYsmPacketProxyImpl.java
@@ -1,29 +1,21 @@
 package meow.kikir.freesia.velocity.network.ysm;
 
-import com.github.retrooper.packetevents.PacketEvents;
-import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
-import com.github.retrooper.packetevents.protocol.player.ClientVersion;
-import com.velocitypowered.api.proxy.Player;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import meow.kikir.freesia.velocity.Freesia;
-import meow.kikir.freesia.velocity.network.mc.NbtRemapper;
-import meow.kikir.freesia.velocity.network.mc.impl.StandardNbtRemapperImpl;
-import meow.kikir.freesia.velocity.utils.FriendlyByteBuf;
 import net.kyori.adventure.key.Key;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
-public class VirtualYsmPacketProxyImpl implements YsmPacketProxy {
-    private final UUID virtualPlayerUUID;
-    private final NbtRemapper nbtRemapper = new StandardNbtRemapperImpl();
-    private volatile NBTCompound lastYsmEntityStatus = null; // TODO Need an access lock like DefaultYsmPacketProxyImpl?
-    private volatile int playerEntityId = -1;
-
+public class VirtualYsmPacketProxyImpl extends YsmPacketProxyLayer {
     public VirtualYsmPacketProxyImpl(UUID virtualPlayerUUID) {
-        this.virtualPlayerUUID = virtualPlayerUUID;
+        super(virtualPlayerUUID);
+    }
+
+    @Override
+    public CompletableFuture<Set<UUID>> fetchTrackerList(UUID observer) {
+        return Freesia.tracker.getCanSee(observer);
     }
 
     @Override
@@ -34,109 +26,5 @@ public class VirtualYsmPacketProxyImpl implements YsmPacketProxy {
     @Override
     public ProxyComputeResult processC2S(Key channelKey, ByteBuf copiedPacketData) {
         return null;
-    }
-
-    @Override
-    public Player getOwner() {
-        return null;
-    }
-
-    @Override
-    public void setEntityDataRaw(NBTCompound data) {
-        this.lastYsmEntityStatus = data;
-    }
-
-    @Override
-    public void refreshToOthers() {
-        final NBTCompound entityStatus = this.lastYsmEntityStatus; // Copy the value
-
-        // If the entity dose not have any data
-        if (entityStatus == null) {
-            return;
-        }
-
-        Freesia.tracker.getCanSee(this.virtualPlayerUUID).whenComplete((beingWatched, exception) -> { // Async tracker check request to backend server
-            if (beingWatched != null) { // Actually there is impossible to be null
-                for (UUID targetUUID : beingWatched) {
-                    final Optional<Player> targetNullable = Freesia.PROXY_SERVER.getPlayer(targetUUID);
-
-                    if (targetNullable.isPresent()) { // Skip send to NPCs
-                        final Player target = targetNullable.get();
-
-                        if (!Freesia.mapperManager.isPlayerInstalledYsm(target)) { // Skip if target is not ysm-installed
-                            continue;
-                        }
-
-                        this.sendEntityStateToInternal(target, entityStatus); // Sync to target
-                    }
-                }
-            }
-        });
-    }
-
-    @Override
-    public NBTCompound getCurrentEntityState() {
-        return this.lastYsmEntityStatus;
-    }
-
-    @Override
-    public void setPlayerWorkerEntityId(int id) {
-        // We are no mapper on the worker
-    }
-
-    @Override
-    public void setPlayerEntityId(int id) {
-        final int oldValue = this.playerEntityId;
-
-        this.playerEntityId = id;
-
-        // State changed
-        if (oldValue != id) {
-            this.refreshToOthers();
-        }
-    }
-
-    @Override
-    public int getPlayerEntityId() {
-        return this.playerEntityId;
-    }
-
-    @Override
-    public int getPlayerWorkerEntityId() {
-        return -1; // No worker entity id
-    }
-
-    @Override
-    public void sendEntityStateTo(@NotNull Player target) {
-        this.sendEntityStateToInternal(target, this.lastYsmEntityStatus);
-    }
-
-    private void sendEntityStateToInternal(Player target, NBTCompound entityStatus) {
-        final int currentEntityId = this.playerEntityId; // Get current entity id on the server of the player
-
-        if (entityStatus == null || currentEntityId == -1) { // If no data got or player is not in the backend server currently
-            return;
-        }
-
-        try {
-            final Object targetChannel = PacketEvents.getAPI().getProtocolManager().getChannel(target.getUniqueId()); // Get the netty channel of the player
-
-            if (targetChannel == null) {
-                return;
-            }
-
-            final ClientVersion clientVersion = PacketEvents.getAPI().getProtocolManager().getClientVersion(targetChannel); // Get the client version of the player
-
-            final int targetProtocolVer = clientVersion.getProtocolVersion(); // Protocol version(Used for nbt remappers)
-            final FriendlyByteBuf wrappedPacketData = new FriendlyByteBuf(Unpooled.buffer());
-
-            wrappedPacketData.writeByte(4);
-            wrappedPacketData.writeVarInt(currentEntityId);
-            wrappedPacketData.writeBytes(this.nbtRemapper.shouldRemap(targetProtocolVer) ? this.nbtRemapper.remapToMasterVer(entityStatus) : this.nbtRemapper.remapToWorkerVer(entityStatus)); // Remap nbt if needed
-
-            this.sendPluginMessageTo(target, YsmMapperPayloadManager.YSM_CHANNEL_KEY_VELOCITY, wrappedPacketData);
-        } catch (Exception e) {
-            Freesia.LOGGER.error("Error in encoding nbt or sending packet!", e);
-        }
     }
 }
---
+++ Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/network/ysm/YsmPacketProxyLayer.java
@@ -0,0 +1,312 @@
+package meow.kikir.freesia.velocity.network.ysm;
+
+import ca.spottedleaf.concurrentutil.util.ConcurrentUtil;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.velocitypowered.api.proxy.Player;
+import io.netty.buffer.Unpooled;
+import meow.kikir.freesia.velocity.Freesia;
+import meow.kikir.freesia.velocity.network.mc.NbtRemapper;
+import meow.kikir.freesia.velocity.network.mc.impl.StandardNbtRemapperImpl;
+import meow.kikir.freesia.velocity.utils.FriendlyByteBuf;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.invoke.VarHandle;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * The framework which is used for all ysm packet proxies</br>
+ * Note:</br>
+ * <p>
+ *     1.This object is only can be used for once, any duplicated entity id and worker entity id updates will be ignored
+ * because it cannot and should not be updated multiple times as we just use them for per mappers</p>
+ * <p>
+ *     2.Any write operations of ysm entity data is ONLY single thread safe!!!</p>
+ *
+ */
+public abstract class YsmPacketProxyLayer implements YsmPacketProxy{
+    protected final Player player;
+
+    protected final UUID playerUUID;
+    protected final NbtRemapper nbtRemapper = new StandardNbtRemapperImpl();
+
+    protected volatile MapperSessionProcessor handler;
+
+    private int playerEntityId = -1;
+    private int workerEntityId = -1;
+
+    private int entityDataReferenceCount = 0; // Used for read-writing lock but writing is always happening on a single thread
+    private NBTCompound lastYsmEntityData = null;
+
+    private boolean proxyReady = false;
+
+    protected static final VarHandle ENTITY_DATA_REF_COUNT_HANDLE = ConcurrentUtil.getVarHandle(YsmPacketProxyLayer.class, "entityDataReferenceCount", int.class);
+    protected static final VarHandle PROXY_READY_HANDLE = ConcurrentUtil.getVarHandle(YsmPacketProxyLayer.class, "proxyReady", boolean.class);
+
+    protected static final VarHandle PLAYER_ENTITY_ID_HANDLE = ConcurrentUtil.getVarHandle(YsmPacketProxyLayer.class, "playerEntityId", int.class);
+    protected static final VarHandle WORKER_ENTITY_ID_HANDLE = ConcurrentUtil.getVarHandle(YsmPacketProxyLayer.class, "workerEntityId", int.class);
+
+    protected static final VarHandle LAST_YSM_ENTITY_DATA_HANDLE = ConcurrentUtil.getVarHandle(YsmPacketProxyLayer.class, "lastYsmEntityData", NBTCompound.class);
+
+    protected YsmPacketProxyLayer(UUID playerUUID) {
+        this.player = Freesia.PROXY_SERVER.getPlayer(playerUUID).orElse(null); // Get if it is a real player
+        this.playerUUID = playerUUID;
+    }
+
+    protected YsmPacketProxyLayer(@NotNull Player player) {
+        this.player = player;
+        this.playerUUID = player.getUniqueId();
+    }
+
+    // Read and write locks for entity data, we just use them for very very short term operations so there is no need to
+    // worry the thread contention issue on performance
+    protected void releaseWriteReference() {
+        ENTITY_DATA_REF_COUNT_HANDLE.setVolatile(this, 0); // There is no any thread contention because the write locks are currently in our hands
+    }
+
+    protected void acquireWriteReference() {
+        int failureCount = 0;
+        for (;;) {
+            for (int i = 0; i < failureCount; i++) {
+                ConcurrentUtil.backoff();
+            }
+
+            final int curr = (int) ENTITY_DATA_REF_COUNT_HANDLE.getVolatile(this);
+
+            // Should not be happened because we are just calling entity data update in a single thread
+            if (curr == -1) {
+                throw new IllegalStateException("Write lock is already held by another thread!");
+            }
+
+            // Reading operations are not finished or another thread is acquiring write or read reference
+            if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, -1)) {
+                failureCount++;
+                continue;
+            }
+
+            break;
+        }
+    }
+
+
+
+    protected void releaseReadReference() {
+        int failureCount = 0;
+        for (;;) {
+            for (int i = 0; i < failureCount; i++) {
+                ConcurrentUtil.backoff();
+            }
+
+            final int curr = (int) ENTITY_DATA_REF_COUNT_HANDLE.getVolatile(this);
+
+            if (curr == -1) {
+                throw new IllegalStateException("Cannot release read reference when write locked");
+            }
+
+            if (curr == 0) {
+                throw new IllegalStateException("Setting reference count down to a value lower than 0!");
+            }
+
+            // Another thread is acquiring read reference
+            if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, curr - 1)) {
+                failureCount++;
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    protected void acquireReadReference() {
+        int failureCount = 0;
+        for (;;) {
+            for (int i = 0; i < failureCount; i++) {
+                ConcurrentUtil.backoff();
+            }
+
+            final int curr = (int) ENTITY_DATA_REF_COUNT_HANDLE.getVolatile(this);
+
+            // Write locked
+            if (curr == -1) {
+                failureCount++;
+                continue;
+            }
+
+            // Another thread is acquiring read or write reference
+            if (!ENTITY_DATA_REF_COUNT_HANDLE.compareAndSet(this, curr, curr + 1)) {
+                failureCount++;
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    // Write and read locks end
+
+    @Nullable
+    @Override
+    public Player getOwner() {
+        return this.player;
+    }
+
+    protected boolean isEntityStateOfSelf(int entityId){
+        final int currentWorkerEntityId = (int) WORKER_ENTITY_ID_HANDLE.getVolatile(this);
+
+        if (currentWorkerEntityId == -1) {
+            return false;
+        }
+
+        return currentWorkerEntityId == entityId;
+    }
+
+    @Override
+    public void setParentHandler(MapperSessionProcessor handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void sendEntityStateTo(@NotNull Player target) {
+        this.acquireReadReference(); // Acquire read reference
+
+        final int currEntityId = (int) PLAYER_ENTITY_ID_HANDLE.getVolatile(this);
+        final NBTCompound currEntityData = (NBTCompound) LAST_YSM_ENTITY_DATA_HANDLE.getVolatile(this);
+
+        this.releaseReadReference(); // Release when we copied the value
+
+        // Not fully initialized yet
+        if (currEntityId == -1 || currEntityData == null) {
+            return;
+        }
+
+        this.sendEntityStateToRaw(target.getUniqueId(), currEntityId, currEntityData);
+    }
+
+    @Override
+    public void setEntityDataRaw(NBTCompound data) {
+        LAST_YSM_ENTITY_DATA_HANDLE.setVolatile(this, data);
+    }
+
+    @Override
+    public void notifyFullTrackerUpdates() {
+        this.acquireReadReference(); // Acquire read reference
+
+        final NBTCompound currEntityData = (NBTCompound) LAST_YSM_ENTITY_DATA_HANDLE.getVolatile(this);
+        final int currEntityId = (int) PLAYER_ENTITY_ID_HANDLE.getVolatile(this);
+
+        this.releaseReadReference(); // Release when we copied the value
+
+        // Not fully initialized yet
+        if (currEntityId == -1 || currEntityData == null) {
+            return;
+        }
+
+        // Prevent race condition
+        if (PROXY_READY_HANDLE.compareAndSet(this, false, true)) {
+            // If we have the mapper connection
+            if (this.handler != null) {
+                // Done queued tracker updates
+                this.handler.retireTrackerCallbacks();
+            }
+        }
+
+        // Sync to the owner self
+        this.sendEntityStateToRaw(this.playerUUID, currEntityId, currEntityData);
+
+        // Fetch can-see list
+        this.fetchTrackerList(this.playerUUID).whenComplete((result, ex) -> {
+            // Exception processing
+            if (ex != null) {
+                Freesia.LOGGER.warn("Failed to fetch tracker list for player uuid {}: {}", this.player != null ? this.player.getUniqueId(): this.playerUUID, ex);
+                return;
+            }
+
+            for (UUID toSend : result) {
+                final Optional<Player> queryResult = Freesia.PROXY_SERVER.getPlayer(toSend);
+
+                // If it is a real player
+                if (queryResult.isPresent()) {
+                    // Check ysm installed
+                    if (Freesia.mapperManager.isPlayerInstalledYsm(toSend)) {
+                        this.sendEntityStateToRaw(toSend, currEntityId, currEntityData);
+                    }
+                }
+            }
+        });
+    }
+
+    public abstract CompletableFuture<Set<UUID>> fetchTrackerList(UUID observer);
+
+    protected void sendEntityStateToRaw(@NotNull UUID receiverUUID, int entityId, NBTCompound data) {
+        try {
+            final Optional<Player> queryResult = Freesia.PROXY_SERVER.getPlayer(receiverUUID);
+
+            if (queryResult.isEmpty()) {
+                return;
+            }
+
+            final Player receiver = queryResult.get();
+            final Object targetChannel = PacketEvents.getAPI().getProtocolManager().getChannel(receiver.getUniqueId()); // Get the netty channel of the player
+
+            if (targetChannel == null) {
+                return;
+            }
+
+            final ClientVersion clientVersion = PacketEvents.getAPI().getProtocolManager().getClientVersion(targetChannel); // Get the client version of the player
+
+            final int targetProtocolVer = clientVersion.getProtocolVersion(); // Protocol version(Used for nbt remappers)
+            final FriendlyByteBuf wrappedPacketData = new FriendlyByteBuf(Unpooled.buffer());
+
+            wrappedPacketData.writeByte(4);
+            wrappedPacketData.writeVarInt(entityId);
+            wrappedPacketData.writeBytes(this.nbtRemapper.shouldRemap(targetProtocolVer) ?
+                    this.nbtRemapper.remapToMasterVer(data) :
+                    this.nbtRemapper.remapToWorkerVer(data)
+            ); // Remap nbt if needed
+
+            this.sendPluginMessageTo(receiver, YsmMapperPayloadManager.YSM_CHANNEL_KEY_VELOCITY, wrappedPacketData);
+        } catch (Exception e) {
+            Freesia.LOGGER.error("Error in encoding nbt or sending packet!", e);
+        }
+    }
+
+    @Override
+    public NBTCompound getCurrentEntityState() {
+        return (NBTCompound) LAST_YSM_ENTITY_DATA_HANDLE.getVolatile(this);
+    }
+
+    @Override
+    public void setPlayerWorkerEntityId(int id){
+        // Only update for once
+        final boolean successfullyUpdated = WORKER_ENTITY_ID_HANDLE.compareAndSet(this, -1, id);
+
+        if (successfullyUpdated) {
+            this.notifyFullTrackerUpdates(); // If it is the first update
+        }
+    }
+
+    @Override
+    public void setPlayerEntityId(int id) {
+        // Only update for once
+        final boolean successfullyUpdated = PLAYER_ENTITY_ID_HANDLE.compareAndSet(this, -1, id);
+
+        if (successfullyUpdated) {
+            this.notifyFullTrackerUpdates(); // If it is the first update
+        }
+    }
+
+    @Override
+    public int getPlayerEntityId() {
+        return (int) PLAYER_ENTITY_ID_HANDLE.getVolatile(this);
+    }
+
+    @Override
+    public int getPlayerWorkerEntityId() {
+        return (int) WORKER_ENTITY_ID_HANDLE.getVolatile(this);
+    }
+}
